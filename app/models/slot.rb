673431442c057bbbe61b79c7e3ff1f860ee0b7bd
Carlos Medeiros <cfmedeiros@ufrn.edu.br>
2023-01-04 22:46:00 -0700

    add get_by_date scope

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,4 +1,6 @@
 class Slot < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  scope :get_by_date, ->(date) { where('start_time <= ? AND end_time>= ?', date.end_of_day, date.beginning_of_day) }
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -3,4 +3,28 @@ require 'rails_helper'
 RSpec.describe Slot, type: :model do
   it { is_expected.to validate_presence_of(:start_time) }
   it { is_expected.to validate_presence_of(:end_time) }
+
+  describe '.get_by_date' do
+    let(:slot) { Slot.create!(start_time: Date.today, end_time: Date.today + 1.hour) }
+
+    it 'returns all slots for a given date' do
+      slot = Slot.create!(start_time: Date.today, end_time: Date.today + 1.hour)
+      expect(described_class.get_by_date(Date.today)).to eq([slot])
+    end
+    context 'when slots ends earlier than the given date' do
+      let(:date) { slot.end_time + 1.day }
+
+      it 'returns not slots' do
+        expect(described_class.get_by_date(date)).to eq([])
+      end
+    end
+
+    context 'when slots starts later than the given date' do
+      let(:date) { slot.start_time - 1.day }
+
+      it 'returns not slots' do
+        expect(described_class.get_by_date(date)).to eq([])
+      end
+    end
+  end
 end
