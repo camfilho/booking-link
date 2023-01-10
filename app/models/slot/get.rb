a836fd8ad05ae2b46880f0755a573467fe8662b0
Carlos Medeiros <cfmedeiros@ufrn.edu.br>
2023-01-09 20:49:56 -0700

    adjust get slots

--- app/models/slot/get.rb
+++ app/models/slot/get.rb
@@ -1,41 +1,39 @@
-  # Purpose: Get slots by date and duration
-  class Slot::Get
-    include ActiveModel::Validations
+# Purpose: Get slots by date and duration
+class Slot::Get
+  include ActiveModel::Validations
 
-    attr_reader :date, :duration
+  attr_reader :date, :duration
 
-    DAY_IN_MINUTES = 1440
-    
-    validates :date, presence: true
-    validates :duration, presence: true
+  DAY_IN_MINUTES = 1440
 
-    validate :duration_must_be_lower_than_3_days
+  validates :date, presence: true
+  validates :duration, presence: true
 
-    def initialize(date:, duration:)
-      @date = date
-      @duration = duration
-    end
+  validate :duration_must_be_lower_than_3_days
 
-    def call
-      return self unless validate
-
-      date_time = DateTime.parse(date)
-      @slots = Slot.get_by_date(date_time)
-
-      ((duration + DAY_IN_MINUTES) / DAY_IN_MINUTES).times do |i|
-        @slots += Slot.get_by_date(date_time + i.day)
-      end
-
-      self
-    end
-
-    def get
-      @slots
-    end
-
-    private
-
-    def duration_must_be_lower_than_3_days
-      errors.add(:duration, 'must be lower than 3 days') if duration > DAY_IN_MINUTES * 3
-    end
+  def initialize(date:, duration:)
+    @date = date
+    @duration = duration
   end
+
+  def call
+    return self unless validate
+
+    start_time = DateTime.parse(date)
+    end_time = start_time + 1.day + duration.minutes
+
+    @slots = Slot.where(start_time: start_time..end_time).or(Slot.where(end_time: start_time..end_time))
+
+    self
+  end
+
+  def get
+    @slots
+  end
+
+  private
+
+  def duration_must_be_lower_than_3_days
+    errors.add(:duration, 'must be lower than 3 days') if duration > DAY_IN_MINUTES * 3
+  end
+end
